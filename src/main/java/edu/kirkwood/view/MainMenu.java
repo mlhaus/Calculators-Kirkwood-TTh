55e6943366eb5f242d6a212ca88c6082bbacaeb1
AsleCoding <aslehagenmc@gmail.com>
2025-10-30 10:39:00 -0500

    Committing my measurement controller and its tests (Asle)

--- src/main/java/edu/kirkwood/view/MainMenu.java
+++ src/main/java/edu/kirkwood/view/MainMenu.java
@@ -43,9 +43,88 @@ public class MainMenu {
                 case 7:
                     EmiliasPythagoreanTheorem.start();
                     break;
+                case 8:
+                    AslesMeasurementCalculator.start();
                 default:
                     return;
             }
         }
+
+
     }
+
+    /**
+     * shows AslesMeasurementCalculator menu
+     */
+    public static void showAsleMenu() {
+        String[] menuItems = {
+                "Create A Measurement",
+                "View Measurement(s)",
+                "Add Measurements",
+                "Subtract Measurements",
+                "Quit"
+        };
+        String menuTitle = "Asle's Measurement Menu";
+        while (true) {
+            printMenu(menuTitle, menuItems);
+            int choice = getInt("Choose an option", false, 1, menuItems.length);
+            switch (choice) {
+                case 1:
+                    AslesMeasurementCalculator.createAMeasurement();
+                    break;
+                case 2:
+                    AslesMeasurementCalculator.viewMeasurements(false);
+                    break;
+                case 3:
+                    AslesMeasurementCalculator.addMeasurement();
+                    break;
+                case 4:
+                    AslesMeasurementCalculator.subtractMeasurements();
+                    break;
+                default:
+                    return;
+
+            }
+        }
+    }
+
+    /**
+     * shows a menu with units, has user enter a number to select a certain unit
+     * @param isMath if used when doing math, removes the back option
+     * @return the integer representing the unit that was selected
+     */
+    public static Number showUnitsMenu(boolean isMath) {
+        Integer returnInt;
+        if (!isMath) {
+            String[] menuItems = {
+                    "Inches",
+                    "Feet",
+                    "Yards",
+                    "Miles",
+                    "Centimeters",
+                    "Meters",
+                    "Kilometers",
+                    "Back"
+            };
+            String menuTitle = "Select A Unit Of Measurement";
+            printMenu(menuTitle, menuItems);
+            returnInt = getInt("Choose an option", true, 1, menuItems.length);
+        }else {
+            String[] menuItems = {
+                    "Inches",
+                    "Feet",
+                    "Yards",
+                    "Miles",
+                    "Centimeters",
+                    "Meters",
+                    "Kilometers",
+            };
+            String menuTitle = "Select A Unit Of Measurement";
+            printMenu(menuTitle, menuItems);
+            returnInt = getInt("Choose an option", true, 1, menuItems.length);
+        }
+
+        return returnInt;
+
+    };
 }
